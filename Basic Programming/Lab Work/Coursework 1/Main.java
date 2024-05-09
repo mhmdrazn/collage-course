33a4a480ac3a2605d5c62f36bea86578a6d80403
Muhammad Razan <140767783+mhmdrazn@users.noreply.github.com>
2024-05-09 09:54:09 +0700

    commit changes

---
+++ Basic Programming/Lab Work/Coursework 1/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Do not change
+        System.out.println("Hello wolrd!");
+        
+        System.out.printf("\n      /\\_____/\\   ");
+        System.out.printf("\n     /  o   o  \\   ");
+        System.out.printf("\n    ( ==  ^  == )   ");
+        System.out.printf("\n     )         (    ");
+        System.out.printf("\n    (           )   ");
+        System.out.printf("\n   ( (  )  (  )  )  ");
+        System.out.printf("\n  (__(__)___(__)__) ");
+            
+    }
+}
