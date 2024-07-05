c1d8957c8a81b39438053ca33322b4099a2322cc
Muhammad Razan <140767783+mhmdrazn@users.noreply.github.com>
2024-07-05 16:44:46 +0700

    Create task5.java

---
+++ Basic Programming/Lab Work/Coursework 4/task5.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void task5(){
+        Scanner sc = new Scanner(Main.class.getResourceAsStream("task5.txt"));
+        String line = sc.nextLine();
+        char[] karakter = new char[line.length()];
+
+        for (int i=0; i< karakter.length; i++){
+            char input = line.charAt(i);
+            if(Character.isLetterOrDigit(input)){
+                karakter[i] = input;
+            }
+        }
+
+        for (int i = 0; i<karakter.length;i++){
+            System.out.print(karakter[i]);
+        }
+    }
+}
