c35ffe97a7c1b3bdde993e8c8a294dc4c4b27df6
Muhammad Razan <140767783+mhmdrazn@users.noreply.github.com>
2024-07-06 08:47:38 +0700

    Create task6.java

---
+++ Basic Programming/Lab Work/Coursework 4/task6.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        task6();
+    }
+
+    public static void task6(){
+        Scanner scan = new Scanner(Main.class.getResourceAsStream("task6.txt"));
+        String input = scan.nextLine();
+        String [] number = input.split(", ");
+    
+        int [] angka = new int[number.length];
+        for (int i = 0;i<number.length;i++){
+            angka[i] = Integer.parseInt(number[i]);
+        }
+    
+        swap(angka, 3,1);
+        swap(angka, 5,7);
+        swapLast(angka, 4,1);
+    
+        for (int i =0; i<angka.length;i++){
+            System.out.print(angka[i]);
+        }
+    }
+}
