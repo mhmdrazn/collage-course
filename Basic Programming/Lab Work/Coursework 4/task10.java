1b14db1cdb4c2a596fd40ec048b9fe943175363e
Muhammad Razan <140767783+mhmdrazn@users.noreply.github.com>
2024-07-09 06:08:54 +0700

    Update task10.java

--- Basic Programming/Lab Work/Coursework 4/task10.java
+++ Basic Programming/Lab Work/Coursework 4/task10.java
@@ -4,36 +4,15 @@ public class Main {
         task10();
     }
 
-    public static void task8(){
-        Scanner scan = new Scanner(Main.class.getResourceAsStream("task8.txt"));
-        int t = scan.nextInt();
-        scan.nextLine();
+    public static void task10(){
+        int m = 1;
+        int s = 36;
+        int h = 12;
 
-        String[] name = new String [t];
-        int[] anime = new int[t];
-        int[] manga = new int[t];
-        Float[] gpa = new Float[t];
-        String [] genre = new String[t];
-
-
-        for (int i = 0; i<t;i++){
-            String input = scan.nextLine();
-            String [] line = input.split(";");
-
-            name[i] = line[0];
-            anime[i] = Integer.parseInt(line[1]);
-            manga[i] = Integer.parseInt(line[2]);
-            gpa[i] = Float.parseFloat(line[3]);
-            genre[i] = line[4];
-        }
-
-        for (int i = 0; i<t;i++){
-            if(gpa[i]<1.0 || anime[i]<10 && manga[i]<10){
-                System.out.println(name[i] + " Expelled");
-            } else if (gpa[i] >= 2.5 || anime[i] >= 100 || manga[i] >= 100){
-                System.out.println(name[i] + " Nihongo Jouzu");
-            } else {
-                System.out.println(name[i] + " Normie");
+        for (int i = s - 1; i > m; i--) {
+            if (i == h) break;
+            if (i % 3 == 0 && i % 2 == 0) {
+                System.out.println(i);
             }
         }
     }
